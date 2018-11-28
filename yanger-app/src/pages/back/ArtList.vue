1bcdab7820739e6c83859cb319a167dd443fdfc5
github-yanger <550799932@qq.com>
2018-11-29 00:41:17 +0800

    文章查询列表优完善

--- yanger-app/src/pages/back/ArtList.vue
+++ yanger-app/src/pages/back/ArtList.vue
@@ -3,22 +3,40 @@
         <div class="container">
             <div class="handle-box">
                 <el-select v-model="artList.type" placeholder="文章类型" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                    <el-option key="1" label="2017年" value="2017"></el-option>
+                    <el-option key="2" label="java相关" value="java"></el-option>
                 </el-select>
                 <el-select v-model="artList.classify" placeholder="文章分类" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
+                    <el-option key="1" label="随笔" value="1"></el-option>
+                    <el-option key="2" label="春" value="spring"></el-option>
                 </el-select>
                 <el-input v-model="artList.queryValue" placeholder="筛选关键词" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="search" @click="search">搜索</el-button>
             </div>
             <el-table :data="artList.page.data" border style="width: 100%" :row-style="{height:'15px'}" 
                 :header-row-style="{'height': '40px'}" ref="multipleTable" stripe>
+                <el-table-column type="expand" label="详情">
+                    <template slot-scope="props">
+                        <el-form label-position="left" inline class="demo-table-expand">
+                            <el-form-item label="作者：">
+                                <span>{{ props.row.author }}</span>
+                            </el-form-item>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <el-form-item label="关键字：">
+                                <span>{{ props.row.ruxWords }}</span>
+                            </el-form-item>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <el-form-item label="图片路径：">
+                                <span>{{ props.row.artImgPath }}</span>
+                            </el-form-item>
+                            <br/>
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="summary">{{ props.row.summary }}</span>
+                        </el-form>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="title" label="标题"></el-table-column>
                 <el-table-column prop="type" label="分类" sortable></el-table-column>
                 <el-table-column prop="classify" label="类型"></el-table-column>
-                <el-table-column prop="title" label="标题"></el-table-column>
-                <el-table-column prop="author" label="作者"></el-table-column>
                 <el-table-column prop="likes" label="喜爱" sortable></el-table-column>
                 <el-table-column prop="views" label="浏览" sortable></el-table-column>
                 <el-table-column prop="commons" label="评论" sortable></el-table-column>
@@ -53,26 +71,27 @@
             }
         },
         created() {
-            this.getPage(1, "", "");
+            this.getPage(1);
         },
         computed: {
         },
         methods: {
             // 分页导航
             handleCurrentChange(val) {
-               this.getPage(val, "", "");
+                this.getPage(val, this.artList.page.pageSize);
             },
             handleSizeChange(val){
-                alert(val)
+                this.getPage(1, val);
             },
            //获取分页数据
-			getPage(pageNo, type, classify) {
+			getPage(pageNo, pageSize) {
 				let _this = this;
 				this.$post("/art/list", {
 					pageNo: pageNo,
+                    pageSize: pageSize,
 					queryValue: _this.artList.queryValue,
-					classify: classify,
-					type: type
+					type: _this.artList.type,
+					classify: _this.artList.classify
 				})
 				.then(function (response) {
 					_this.artList.page = response.data;
@@ -83,16 +102,13 @@
             },
             //搜索
             search() {
-                
+                this.getPage(1);
             },
             //时间格式化
             dateFormatter(row, column) {
                 var date = new Date(row.updateTime);
                 return formatDate(date, 'yyyy-MM-dd hh:mm');
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
-            },
+            }
         }
     }
 </script>
@@ -122,6 +138,9 @@
         word-break: break-all;
         line-height: 2px;
     }
+    .summary {
+        line-height: 20px;
+    }
 </style>
 
 <style>
@@ -131,5 +150,8 @@
         white-space: normal;
         word-break: break-all;
         line-height: 16px;
+    }
+    .el-form-item--small .el-form-item__content, .el-form-item--small .el-form-item__label {
+        line-height: 18px;
     }
 </style>
